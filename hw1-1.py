3435b904f1774200e163fe2321899776e9b25a7e
zapadlo7 <zapadlo222@gmail.com>
2024-03-04 00:35:56 +0200

    fix

--- hw1-1.py
+++ hw1-1.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 
 def get_birthdays_per_week(users):
-    # Поточна дату
+    # Поточна дата
     today = datetime.today().date()
 
     # Словник для зберігання днів народження користувачів за днем тижня
@@ -18,16 +18,16 @@ def get_birthdays_per_week(users):
         if birthday_this_year < today:  # Якщо день народження вже був цього року
             birthday_this_year = birthday_this_year.replace(year=today.year + 1)
 
+        # Переносимо день народження з суботи або неділі на понеділок
+        if birthday_this_year.weekday() in [5, 6]:  # 5 - субота, 6 - неділя
+            birthday_this_year += timedelta(days=(7 - birthday_this_year.weekday()))
+
         # Порівняння з Поточною Датою
         delta_days = (birthday_this_year - today).days
 
-        # Визначення Дня Тижня
-        if delta_days < 7:  # Якщо день народження в наступному тижні
+        # Додаємо користувача до відповідного дня тижня
+        if delta_days < 7:  # Якщо день народження в наступних 7 днях
             birthday_weekday = birthday_this_year.strftime("%A")
-            # Користувачів, у яких день народження був на вихідних, привітати в понеділок
-            if birthday_weekday in ["Saturday", "Sunday"]:
-                birthday_weekday = "Monday"
-            # Додаємо користувача до відповідного дня тижня
             birthdays_per_week[birthday_weekday].append(name)
 
     # Виводимо список користувачів, яких потрібно привітати на наступному тижні
@@ -38,14 +38,13 @@ def get_birthdays_per_week(users):
         day_of_week = next_day.strftime("%A")
         if birthdays_per_week[day_of_week]:
             print(f"{day_of_week}: {', '.join(birthdays_per_week[day_of_week])}")
-        
 
 # Приклад використання
 users = [
-    {"name": "Bill Gates", "birthday": datetime(1955, 3, 8)},
+    {"name": "Bill Gates", "birthday": datetime(1955, 3, 3)},
     {"name": "Steve Jobs", "birthday": datetime(1955, 3, 4)},
-    {"name": "Mark Zuckerberg", "birthday": datetime(1984, 3, 3)},
-    {"name": "Kim Kardashian", "birthday": datetime(1975, 3, 7)}
+    {"name": "Mark Zuckerberg", "birthday": datetime(1984, 3, 5)},
+    {"name": "Kim Kardashian", "birthday": datetime(1975, 3, 9)}
 ]
 
 get_birthdays_per_week(users)
